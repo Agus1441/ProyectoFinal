5e7ca3a2ce08a97bd38215bc2392106b2b49c351
DiazFede <diazfedericoucu@gmail.com>
2024-10-21 10:14:10 -0300

    -

--- Frontend/src/Components/Register/Modal.jsx
+++ Frontend/src/Components/Register/Modal.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-// src/components/RegisterModal.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Importar useNavigate
 import './Modal.css';
