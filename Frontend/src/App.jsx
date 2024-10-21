406404e59047eaa2a087d5d011c339b403d4af9a
Agus1441 <agustin.schlechter@correo.ucu.ed>
2024-10-21 09:11:26 -0300

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Login from './Pages/Login/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login/Login'
 import Register from './Pages/Register/Register'
