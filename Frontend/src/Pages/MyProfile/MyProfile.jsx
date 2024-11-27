823f9d583ba62686e2b68a799cabc17630ef7d37
Agus1441 <agustin.schlechter@correo.ucu.ed>
2024-11-27 08:35:29 -0300

    cambio

--- Frontend/src/Pages/MyProfile/MyProfile.jsx
+++ Frontend/src/Pages/MyProfile/MyProfile.jsx
@@ -90,7 +90,7 @@ const MyProfile = () => {
             <div className="profile-header">
                 <div className="profile-header-content">
                     <button className="upload-button" onClick={openModal}>
-                        <img src="https://static-00.iconduck.com/assets.00/upload-icon-2048x2048-eu9n5hco.png" alt="upload" className="icon upload-icon" />
+                        <img src="https://pic.onlinewebfonts.com/thumbnails/icons_193050.svg" alt="upload" className="icon upload-icon" />
                     </button>
                     <MoreOptions />
                 </div>
